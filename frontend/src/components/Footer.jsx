efcc088632dc5c1582909593854501603e391567
janakaprasadq <janakaprasad.style@gmail.com>
2025-04-18 14:49:31 +0530

    Edit footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
       <div>
         <hr />
         <p className="py-5 text-sm text-center">
-          Copyright 2025@ forever.com - All Right Recerved.
+          Copyright 2025@ threadloom-wfyw.vercel.app - All Right Recerved.
         </p>
       </div>
     </div>
